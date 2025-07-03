ff166b945fee4893421debd60ba743ad10d6b8ae
467980 <benedickcorpuz274@gmail.com>
2025-07-03 21:35:19 +0800

    Update booleans.js

--- booleans.js
+++ booleans.js
@@ -1,1 +1,11 @@
+let firstFavNumb = 14;
+let secondFavNumb = 15;
 
+console.log(firstFavNumb + " > " + secondFavNumb + " = " + (firstFavNumb > secondFavNumb));
+console.log(firstFavNumb + " < " + secondFavNumb + " = " + (firstFavNumb < secondFavNumb));
+console.log(firstFavNumb + " >= " + secondFavNumb + " = " + (firstFavNumb >= secondFavNumb));
+console.log(firstFavNumb + " <= " + secondFavNumb + " = " + (firstFavNumb <= secondFavNumb));
+console.log(firstFavNumb + " === " + secondFavNumb + " = " + (firstFavNumb === secondFavNumb));
+console.log(firstFavNumb + " == " + secondFavNumb + " = " + (firstFavNumb == secondFavNumb));
+console.log(firstFavNumb + " !== " + secondFavNumb + " = " + (firstFavNumb !== secondFavNumb));
+console.log(firstFavNumb + " != " + secondFavNumb + " = " + (firstFavNumb != secondFavNumb));
