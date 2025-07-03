d3c77c8eb64b6f2d33652aef3113bbfb3b91756b
467980 <benedickcorpuz274@gmail.com>
2025-07-03 21:25:17 +0800

    Update comparison.js

--- comparison.js
+++ comparison.js
@@ -1,1 +1,9 @@
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 7;
+let result = favNumb + undefined;
 
+console.log("Is JavaScript programming language", isJsProgrammingLanguage);
+console.log("Is KavaScript hard", isJsHard);
+console.log("Favorite number:", favNumb);
+console.log("Favorite number + undefined:", result);
