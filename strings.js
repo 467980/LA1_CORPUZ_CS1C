44de4941e4ce9dc7038c872859e68a228f98461c
467980 <benedickcorpuz274@gmail.com>
2025-07-03 21:41:12 +0800

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,1 +1,13 @@
+let favActorFirstName = "Doctor";
+let favActorLastName = "Strange";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = "My favorite actor is " + uppercase;
 
+message += " and his best movie is Doctor Strange in the Multiverse of Madness.";
+
+console.log("First Name: " + favActorFirstName);
+console.log("Last Name: " + favActorLastName);
+console.log("Full Name: " + fullName);
+console.log("Uppercase: " + uppercase);
+console.log("Message: " + message);
